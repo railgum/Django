6dc60dc9cb309c6de5c2b6347677d83c093b4bc9
Rail <railbolo@gmail.com>
2024-02-16 21:28:27 +0400

    всё ещё пытаюсь понять many-to-many field

--- lesson_2_Models/hw/shopapp/management/commands/create_order.py
+++ lesson_2_Models/hw/shopapp/management/commands/create_order.py
@@ -1,21 +1,28 @@
 from django.core.management.base import BaseCommand
 from shopapp.models import Client, Product, Order
 from random import choice, randint
+from django.db.models import Sum
 
 
 class Command(BaseCommand):
     help = 'Create orders'
+
     def add_arguments(self, parser):
         parser.add_argument('id', type=int, help='Client ID')
 
     def handle(self, *args, **options):
         products = Product.objects.all()
         pk = options.get('id')
-        order = Order.objects.create(client_id=pk)
-        amount = 0
+        order = Order(
+            client_id=pk,
+            amount=100,
+        )
+        print(products.filter(pk=order.products).aggregate(Sum('price')))
+        order.save()
+        # amount = 0
         for _ in range(1, randint(1, 5)):
-            order.product_set.add(choice(products))
-            self.stdout.write(order.product.price)
-            amount += order.product.price
-        order.amount = amount
+            order.products_id.add(choice(products.pk))
+            self.stdout.write(order)
+            # amount += order.products.price
+        # order.amount = products.filter(pk=order.products_id).aggregate(Sum('price'))
         order.save()
