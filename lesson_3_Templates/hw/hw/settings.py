cd3bdea0be9ba75fe91c57616c01d6f90da79fcb
Rail <railbolo@gmail.com>
2024-02-18 16:57:56 +0400

    new in hw 3

--- lesson_3_Templates/hw/hw/settings.py
+++ lesson_3_Templates/hw/hw/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop_temp_app',
+    'shop_template_app',
 ]
 
 MIDDLEWARE = [
--- lesson_3_Templates/hw/shop_template_app/apps.py
+++ lesson_3_Templates/hw/shop_template_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ShopTempAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'shop_temp_app'
+    name = 'shop_template_app'
