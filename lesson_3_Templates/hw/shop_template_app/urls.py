4fbb0e380341f195ac008129df4ac0a3e974ff04
Rail <railbolo@gmail.com>
2024-02-22 22:21:39 +0400

    end hw 3

--- lesson_3_Templates/hw/shop_template_app/urls.py
+++ lesson_3_Templates/hw/shop_template_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 
-urlpatterns=[
-    path('')
+urlpatterns = [
+    path('orders/<int:user_id>/', client_all_orders, name='show_orders_clients'),
+    path('orders_by_date/<int:client_id>', orders_by_date, name='show_orders_by_date'),
 ]
